12eec3d55cf41ea1271057ffe402b575c9406b06
Andrey Helldar <helldar@dragon-code.pro>
2022-12-28 01:57:23 +0300

    Added samplers from 2.5.3

--- config/samplers.php
+++ config/samplers.php
@@ -3,12 +3,18 @@
 declare(strict_types=1);
 
 return [
-    'plms',
-    'lms',
     'ddim',
-    'heun',
-    'euler',
-    'euler_a',
     'dpm2',
     'dpm2_a',
+    'dpm_adaptive',
+    'dpm_fast',
+    'dpm_solver_stability',
+    'dpmpp_2m',
+    'dpmpp_2s_a',
+    'dpmpp_sde',
+    'euler',
+    'euler_a',
+    'heun',
+    'lms',
+    'plms',
 ];
